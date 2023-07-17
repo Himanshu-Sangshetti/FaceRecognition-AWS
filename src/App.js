4550d1369fec107cbf8100b6278b626f961c28e3
Himanshu <104683257+Himanshu-Sangshetti@users.noreply.github.com>
2023-07-17 23:14:50 +0530

    Intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
+const uuid=require('uuid');
+
 function App() {
+
+  const [image,setImage]=useState('');
+  const [uploadResultMessage,setUploadResultMessage]=useState('Please enter an image to authenticate');
+  const [visitorName,setVisitorName]=useState('placeholder.jpeg');
+  const[isAuth,setAuth]=useState(false);
+
+  function sendImage(e){
+    e.preventDefault();
+    setVisitorName(image.name);
+    const visitorImageName=uuid.v4();
+    fetch(`https://8hpjvto9t1.execute-api.ap-south-1.amazonaws.com/dev/visitors-images/${visitorImageName}.jpeg` ,{
+    
+    method:'PUT',
+      headers:{
+        'Content-Type':'image/jpeg'
+      },
+      body:image
+    }).then(async()=>{
+      const response=await authenticate(visitorImageName);
+      if(response.Message==='Success'){
+        setAuth(true);
+        setUploadResultMessage(`Hi ${response['firstName']} ${response['lastName']},welcome to work`)
+      }else{
+        setAuth(false);
+        setUploadResultMessage(`Authentication Failed`)
+      }
+    }).catch(error=>{
+      setAuth(false);
+      setUploadResultMessage(`There is an error in authentication process,try again llater.`)
+      console.error(error);
+    })
+
+  }
+
+  async function authenticate(visitorImageName){
+    const requestUrl='https://8hpjvto9t1.execute-api.ap-south-1.amazonaws.com/dev/employee?'+new URLSearchParams({
+      objectKey:`${visitorImageName}.jpeg`
+    });
+    return await fetch(requestUrl,{
+
+      method:'GET',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json'
+      }
+    }).then(response=>response().json)
+    .then((data)=>{
+      return data;
+    }).catch(error=>console.error(error));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h2>FACIAL RECOGNITION SYSTEM</h2>
+    <form onSubmit={sendImage}>
+      <input type='file' name='image' onChange={e=> setImage(e.target.files[0])} />
+      <button type='submit'>Authenticate</button>
+    </form>
+
+    <div className={isAuth? 'success':'failure'}>{uploadResultMessage}</div>
+    <img src={require(`./visitors/${visitorName}`)} alt='Visitor' height={250} width={250} />
     </div>
   );
 }
